8c7dcc8d80afef4109b2e2069f5f5b35d0eaf252
Daniel Kolesnik <daneelkolesnik@gmail.com>
2020-08-02 05:57:04 -0700

    [+] initialized spring boot application;

---
+++ src/main/java/com/dkolesnik/exampful/ExampfulApplication.java
@@ -0,0 +1,13 @@
+package com.dkolesnik.exampful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampfulApplication.class, args);
+	}
+
+}
